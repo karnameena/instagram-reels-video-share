33ededc073f24432f1948d1b121a148f22c50c49
karnameena <154045441+karnameena@users.noreply.github.com>
2024-06-08 13:29:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -337,6 +337,16 @@ app.post("/camsnap", (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("App Running on Port 5000!");
-});
+// app.listen(5000, () => {
+//   console.log("App Running on Port 5000!");
+// });
+
+
+
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
+
